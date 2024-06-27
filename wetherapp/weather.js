6fb6efdc97a450a99e9e67b7e76ccdabef5ca582
dharmik31 <121918467+Dharv31@users.noreply.github.com>
2024-06-27 15:06:09 +0530

    Add files via upload

---
+++ wetherapp/weather.js
@@ -0,0 +1,28 @@
+const apiKey="6713f7c483b7640961d9d1aa28a3426c";
+
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    
+ 
+
+}
+
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
+});
+
